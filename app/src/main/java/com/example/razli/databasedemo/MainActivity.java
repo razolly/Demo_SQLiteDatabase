50320c2c7f3951e8dd0c6455ccfe5e614c6cef49
Raz <razli1993@hotmail.com>
2018-06-22 17:56:49 +0800

    Surrounded with try catch

--- app/src/main/java/com/example/razli/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/razli/databasedemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.razli.databasedemo;
 
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,31 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase mDatabase = this.openOrCreateDatabase("Cats", MODE_PRIVATE, null);
+        try {
+            SQLiteDatabase mDatabase = this.openOrCreateDatabase("Cats", MODE_PRIVATE, null);
 
-        // Create new table in database
-        mDatabase.execSQL("CREATE TABLE IF NOT EXISTS cats (name VARCHAR, age INT(3))");
+            // Create new table in database
+            mDatabase.execSQL("CREATE TABLE IF NOT EXISTS cats (name VARCHAR, age INT(3))");
 
-        // Add to database
-        mDatabase.execSQL("INSERT INTO cats (name, age) VALUES ('Rocky', 14)");
-        mDatabase.execSQL("INSERT INTO cats (name, age) VALUES ('Tabbie', 7)");
+            // Add to database
+            mDatabase.execSQL("INSERT INTO cats (name, age) VALUES ('Rocky', 14)");
+            mDatabase.execSQL("INSERT INTO cats (name, age) VALUES ('Tabbie', 7)");
 
-        Cursor resultSet = mDatabase.rawQuery("SELECT * FROM cats", null);
+            Cursor resultSet = mDatabase.rawQuery("SELECT * FROM cats", null);
 
-        int nameIndex = resultSet.getColumnIndex("name");
-        int ageIndex = resultSet.getColumnIndex("age");
+            int nameIndex = resultSet.getColumnIndex("name");
+            int ageIndex = resultSet.getColumnIndex("age");
 
-        // Move cursor to the starting position to start reading data
-        resultSet.moveToFirst();
+            // Move cursor to the starting position to start reading data
+            resultSet.moveToFirst();
 
-        while(resultSet != null) {
-            Log.i(TAG, "onCreate: Name: " + resultSet.getString(nameIndex));
-            Log.i(TAG, "onCreate: Age: " + resultSet.getString(ageIndex));
+            while (resultSet != null) {
+                Log.i(TAG, "onCreate: Name: " + resultSet.getString(nameIndex));
+                Log.i(TAG, "onCreate: Age: " + resultSet.getString(ageIndex));
 
-            // Moves to next row
-            resultSet.moveToNext();
+                // Moves to next row
+                resultSet.moveToNext();
+            }
+
+            resultSet.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        resultSet.close();
     }
 }
